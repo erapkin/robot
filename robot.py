92721104d9e901b4675d351bb2f72e8d9bfa420e
unknown <erapkin@dev1.bsecode.inc>
2015-09-14 14:59:12 -0500

    added list of punches, added multi punch loop, added user input to kick off loop

--- robot.py
+++ robot.py
@@ -1,13 +1,27 @@
+import random
+
 class Robot :
 	
 	def __init__(self, powerSupply):
 		self.powerSupply = powerSupply
 		
-	def punch(self, amount):
-		self.powerSupply.drain(amount)
-		print "You punch. Kapow!"
 		
-	# def drainOpponent():
+	def punch(self, amount=3):
+		punchList = ['Doing the Robot on yo face', 'Jackie Chan Strike', '"what is that?" punches face', 'Uppercut', 'B@#$Slap','WubWubWubWubWub', 'Moonwalk all over then STRIKE']
+		
+		print self.powerSupply.powerBalance
+		if  (self.powerSupply.powerBalance > 2):
+			self.powerSupply.drain(amount)
+			print (random.choice(punchList))
+		else:
+			print "Insufficient power to punch"
+			exit(0)
+			
+	def multiplePunch(self, numberOfPunches):
+		for x in range (0,numberOfPunches):
+			self.punch()	
+	
+	# def chargedrain():
 
 class LaserRobot(Robot):
 
@@ -19,6 +33,7 @@ class NinjaRobot(Robot):
 	
 	def slice(self,amount):
 		self.powerSupply.drain(amount)
+
 		
 class PowerSupply :
 		
@@ -26,7 +41,10 @@ class PowerSupply :
 		self.powerBalance = powerBalance
 		
 	def drain (self, amount):
-		self.powerBalance -= amount
+		if (self.powerBalance > 0):
+			self.powerBalance -= amount
+		else:
+			print "insufficient power"
 		
 	def printPowerBalance (self):
 		print self.powerBalance 
@@ -36,16 +54,20 @@ class RechargableBattery(PowerSupply):
 	def charge (self, amount):
 		self.powerBalance += amount
 		
-
-			
 duracelBattery = RechargableBattery(100)
-energizerBattery = RechargableBattery(100)
+# energizerBattery = RechargableBattery(100)
 shinobi = Robot(duracelBattery)
 
-shinobi.punch(5)
+number = raw_input('How many punches?:')
+shinobi.punch()
+shinobi.multiplePunch(int(number))
+
+print "======================================="
+print "current power balance: "
 duracelBattery.printPowerBalance()
-shinobi.punch(4)
-duracelBattery.printPowerBalance()
+
+
+
 
 # duracelBattery.drain (5)
 # duracelBattery.charge (15)
